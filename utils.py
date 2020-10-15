fed797bea7e47e5e5713a2bf81d47852d92a564d
berfinsimsek <37277437+berfinsimsek@users.noreply.github.com>
2020-10-15 13:37:51 +0200

    the repo is cleaned up

--- utils.py
+++ utils.py
@@ -3,29 +3,45 @@ import matplotlib.pyplot as plt
 from numpy import linalg as LA
 from scipy.optimize import fsolve
 
+def solve_eff_ridge(D, P, lambd, lambd0 = 1e-8):
+    # for given eigenvalues D, number of features P, and ridge lambd,
+    # returns the effective ridge and its derivative
+    N = D.size
+    def f(z):
+        s = 0;
+        for i in range(N):
+            s += D[i] / (D[i] + z)
+        return P * (1 - lambd / z) - s
+    tlambd = fsolve(f, lambd0).item()
+    s2 = 0
+    for i in range(N):
+        s2 += D[i] ** 2 / (D[i] + tlambd) ** 2
+    der_tlambd = 1 / (1 - s2 / P)
+    return tlambd, der_tlambd
+
 def make_eig_spect(N, exp=True, exp_decay=0.5, pol_decay=1):
     # makes an eigenvalue spectrum (of the Kernel) with desired decay properties:
-    # flag=1 for the exponential decay with exp(-beta*(i-1)), and
-    # flag=0 is for the polynomial decay with i**(-alpha) for i=1,...,N.
-    if(exp): 
+    # exp = 1 for the exponential decay with exp(-beta*(i-1)), and
+    # exp = 0 is for the polynomial decay with i**(-alpha) for i = 1,...,N.
+    if(exp):
         return np.exp(-exp_decay*np.linspace(0,N-1,N))
-    else: 
-        return np.linspace(1,N,N)**(-pol_decay)     
+    else:
+        return np.linspace(1,N,N)**(-pol_decay)
 
-def calc_emp_eigs(D, P, lambd, num_trials=1000):
-    # for given eigenvalue spectrum D and a ridge lambd, calculates the empirical average and variance   
-    # of the diagonal entries of A_\lambda for P=1,...,N. 
+def calc_emp_eigs(D, P, lambd, num_trials = 1000):
+    # for given eigenvalue spectrum D and a ridge lambd, returns the empirical average and variance
+    # of the diagonal entries of A_\lambda for P=1,...,N.
     N = D.size
     A = np.zeros((N, num_trials))
     for it in range(num_trials):
-        W = np.random.normal(0,1,size=(N,P))
-        B = np.linalg.inv(np.matmul(np.matmul(W.T,np.diag(D)),W)/P + lambd*np.eye(P))
+        W = np.random.normal(0, 1, size = (N, P))
+        B = np.linalg.inv(np.matmul(np.matmul(W.T, np.diag(D)), W) / P + lambd * np.eye(P))
         for i in range(N):
-            A[i, it] = D[i]*np.inner(W[i,:],np.matmul(B, W[i,:]))/P
-    return np.mean(A, axis=1), np.sqrt(np.var(A, axis=1))
+            A[i, it] = D[i] * np.inner(W[i,:], np.matmul(B, W[i,:])) / P
+    return np.mean(A, axis = 1), np.sqrt(np.var(A, axis = 1))
 
 def calc_kernel_eigs(D, lambd):
-    # for given eigenvalue spectrum D and a ridge lambd, 
+    # for given eigenvalue spectrum D and a ridge lambd,
     # returns the effective eigenvalue spectrum
     N = D.size
     C = np.zeros(N)
@@ -33,27 +49,54 @@ def calc_kernel_eigs(D, lambd):
         C[i] = D[i] / (D[i] + lambd)
     return C
 
-def solve_eff_ridge(D, P, lambd):
-    # for given eigenvalues D, number of features P, and ridge lambd,
-    # numerically solves for the effective ridge.
-    N = D.size
-    def f(z):
-        s = 0;
-        for i in range(N):
-            s += D[i] / (D[i] + z) 
-        return P*(1-lambd/z) - s
-    sol = np.asscalar(fsolve(f, 1e-4))
-    eff_ridge = max(sol, 0) # if removed small negative solutions can be found due to numerical precision
-    eff_dim2 = 0
-    for i in range(N):
-        eff_dim2 += D[i]**2 / (D[i]+eff_ridge)**2
-    eff_ridge_der = 1 / (1 - eff_dim2 / P)    
-    return eff_ridge, eff_ridge_der
+def pred(X1, X2, Y1, P, lambd, c, W = 1):
+    # for given training input and output data (X1, Y1), returns sample predictions
+    # of the RF predictor on the training and test data (X1, X2)
+    # c chooses the kernel type, see the function gram_matrix and W is the lengthscale
+    N = X1.shape[0]
+    N2 = X2.shape[0]
+    X_all = np.concatenate((X1, X2), axis=0)
+    G = gram_matrix(X_all, W, flag = c)
+    D, U = np.linalg.eig(G);
+    D[D < 0] = 0; D = D.real;
+    R = np.matmul(U, np.diag(np.sqrt(D)))
+    F_all = np.matmul(R, np.random.normal(0, 1, size=(N + N2, P))) # GP features
+    F1 = F_all[:X1.shape[0], :];
+    F2 = F_all[X1.shape[0]:, :];
+    B = np.linalg.pinv(np.matmul(F1.T, F1) / P + lambd * np.eye(P))
+    theta = np.matmul(np.matmul(B, F1.T), Y1) # the optimal parameter
+    Y1_pred = np.matmul(F1, theta) / P
+    Y2_pred = np.matmul(F2, theta) / P
+    return Y1_pred, Y2_pred
+
+def gram_matrix(X, W, flag = 1):
+    # returns the kernel gram matrix for a given dataset X with three Kernel options
+    def ker(x1, x2, W):
+        d = np.linalg.norm(x1 - x2)
+        if flag == 1: # RBF Kernel
+            return np.exp( -d**2 / W)
+        if flag == 0: # Mat3/2 Kernel
+            return (1 + (np.sqrt(3) * d) / W) * np.exp(-(np.sqrt(3) * d) / W)
+        if flag == -1: # Mat5/2 Kernel
+            return (1 + (np.sqrt(5) * d) / W + (np.sqrt(5) * d**2) / (3 * W**2)) * np.exp(-(np.sqrt(5) * d) / W)
+
+    G = np.zeros((X.shape[0], X.shape[0]))
+    for i1 in range(X.shape[0]):
+        for i2 in range(X.shape[0]):
+            if(X.ndim == 1):
+                G[i1,i2] = ker(X[i1], X[i2], W)
+            else:
+                G[i1,i2] = ker(X[i1,:], X[i2,:], W)
+    return G
+
+
+########## the rest is only used in the older versions of the code. #########################
+
 
 def calc_eff_ridges(D, lambd_list, P_list):
     # for given eigenvalues D, and a list of ridge parameters lambd_list,
     # and of number of features P, calculates the corresponding effective ridge and its derivative.
-    N = D.size; 
+    N = D.size;
     num_lambd = lambd_list.size;
     num_P = P_list.size;
     E = np.zeros((num_lambd, num_P));
@@ -64,39 +107,5 @@ def calc_eff_ridges(D, lambd_list, P_list):
             P = P_list[j]
             eff_ridge, eff_ridge_der = solve_eff_ridge(D,P,lambd)
             E[i, j] = eff_ridge
-            E_der[i, j] = eff_ridge_der  
+            E_der[i, j] = eff_ridge_der
     return E, E_der
-
-def pred(X1, X2, Y1, P, lambd):
-    # for given training input and output data (X1, Y1), returns sample predictions 
-    # for the linear regression with Gaussian features on the training and test data (X1, X2)
-    X_all = np.concatenate((X1, X2), axis=0)
-    C = make_cov_matrix(X_all)
-    D, U = np.linalg.eig(C); 
-    D[D < 0] = 0; D = D.real; # fixing small numerical perturbations
-    R = np.matmul(U, np.diag(np.sqrt(D)))
-    F_all = np.matmul(R, np.random.normal(0, 1, size=(X_all.shape[0], P))) # sample Gaussian features
-    F1 = F_all[:X1.shape[0], :]; 
-    F2 = F_all[X1.shape[0]:, :];
-    B = np.linalg.pinv(np.matmul(F1.T, F1) / P + lambd*np.eye(P))
-    theta = np.matmul(np.matmul(B, F1.T), Y1) # the optimal parameter 
-    Y1_pred = np.matmul(F1, theta) / P
-    Y2_pred = np.matmul(F2, theta) / P 
-    return Y1_pred, Y2_pred
-
-def make_cov_matrix(X):
-    # making a covariance matrix for a given dataset with the RBF Kernel
-    def RBF_ker(x1, x2, l=1):
-        # RBF Kernel function
-        return np.exp(-np.linalg.norm(x1-x2)**2/(2*l))
-    
-    C = np.zeros((X.shape[0], X.shape[0])) 
-    for i1 in range(X.shape[0]):
-        for i2 in range(X.shape[0]):
-            if(X.ndim == 1):
-                C[i1,i2] = RBF_ker(X[i1], X[i2])
-            else:
-                C[i1,i2] = RBF_ker(X[i1,:], X[i2,:])
-    return C
-
-
--- utils_MNIST.py
+++ utils_MNIST.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
+from utils import *
 
 def normalize_data(X, cut=2):
-    X2 = np.zeros((X.shape[0], (28-2*cut)**2))
+    # normalising the MNIST dataset (X)
+    Xnorm = np.zeros((X.shape[0], (28-2*cut)**2))
     for i in range(X.shape[0]):
         im = np.reshape(X[i][:], (28,28))
         im_cut = im[cut:28-cut, cut:28-cut].flatten()
-        X2[i][:] = im_cut / 255 - np.mean(im_cut / 255)
-    return X2
+        Xnorm[i][:] = im_cut / 255 - np.mean(im_cut / 255)
+    return Xnorm
 
 def make_two_classes(label1, label2, X, Y, X2, Y2):
+    # given training (X,Y) and test datasets (X2, Y2), we choose two classes,
+    # apply normalization and return the (modified) training and test datasets
     c7 = Y == label1; c9 = Y == label2;
     Y_trn = np.concatenate((np.ones(Y[c7].shape), -np.ones(Y[c9].shape)))
     X_trn = np.concatenate((X[c7][:], X[c9][:]), axis=0) 
@@ -22,32 +26,11 @@ def make_two_classes(label1, label2, X, Y, X2, Y2):
     X_trn = normalize_data(X_trn); X_tst = normalize_data(X_tst); 
     return X_trn, Y_trn, X_tst, Y_tst
 
-def gauss_ker(x1,x2,l=0.1):
-    return np.exp(-np.linalg.norm(x1-x2)**2/(2*l))
-
-def make_cov_matrix(X):
-    C = np.zeros((X.shape[0], X.shape[0])) # covariance matrix
-    for i1 in range(X.shape[0]):
-        for i2 in range(X.shape[0]):
-            C[i1,i2] = gauss_ker(X[i1,:], X[i2,:])
-    return C
-
-def pred(X1, X2, Y1, Y2, P):
-    X_all = np.concatenate((X1, X2), axis=0)
-    C = make_cov_matrix(X_all)
-    F_all = np.zeros((X_all.shape[0], X1.shape[1])) # make random features 
-    D, U = np.linalg.eig(C); D[D < 0] = 0;
-    R = np.matmul(U, np.diag(np.sqrt(D)))
-    F_all = np.matmul(R, np.random.normal(0, 1, size=(X_all.shape[0],P)))
-    F1 = F_all[:X1.shape[0],:]; F2 = F_all[X1.shape[0]:,:];
-    B = np.linalg.pinv(np.matmul(F1.T, F1)) 
-    theta = np.matmul(np.matmul(B, F1.T), Y1) # the optimal predictor 
-    Y1_pred = np.matmul(F1, theta); Y2_pred = np.matmul(F2, theta) # predictions on training and test data
-    return Y1_pred, Y2_pred
-
 def KernelPredictor(X, Y, X2, lambd=0):
-    # for lambd /neq 0, non-interpolating solutions on training data should be coded
-    C = make_cov_matrix(X); C_inv = np.linalg.inv(C + lambd*np.eye(X.shape[0]))
+    # given training dataset (X, Y), test inputs X2, and ridge lambd,
+    # returns the predictions of the Kernel function on the test input 
+    C = make_cov_matrix(X); 
+    C_inv = np.linalg.inv(C + lambd*np.eye(X.shape[0]))
     Y_pred = np.zeros(X2.shape[0])
     for i in range(X2.shape[0]):
         x = X2[i,:]
@@ -58,6 +41,7 @@ def KernelPredictor(X, Y, X2, lambd=0):
     return Y_pred
 
 def expectedRFPredictor(X, X2, Y, Y2, P, lambd=0, num_trials = 50):
+    # returns some qualitative metrics on test input data
     Y1_all = np.zeros((Y.size, num_trials))
     Y2_all = np.zeros((Y2.size, num_trials))
     aver_trn_err = 0; 
